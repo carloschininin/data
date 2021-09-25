d746dcbd719832a9bc17d842f6ec0fd1674cb5c5
Carlos Chininin <carloschininin@gmail.com>
2021-09-25 14:20:50 -0500

    [export] add alignment style.

---
+++ src/Export/AlignmentStyle.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the PIDIA
+ * (c) Carlos Chininin <cio@pidia.pe>
+ */
+
+namespace CarlosChininin\Data\Export;
+
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+
+final class AlignmentStyle
+{
+    public const HORIZONTAL_LEFT = Alignment::HORIZONTAL_LEFT;
+    public const HORIZONTAL_RIGHT = Alignment::HORIZONTAL_RIGHT;
+    public const HORIZONTAL_CENTER = Alignment::HORIZONTAL_CENTER;
+    public const VERTICAL_BOTTOM = Alignment::VERTICAL_BOTTOM;
+    public const VERTICAL_TOP = Alignment::VERTICAL_TOP;
+    public const VERTICAL_CENTER = Alignment::VERTICAL_CENTER;
+}
